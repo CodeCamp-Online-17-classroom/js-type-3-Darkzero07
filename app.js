aaca6ea8c5bb8a78874492eaaa3fd3a304fdad5b
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-18 20:56:25 +0700

    Lab12 Dynamic Type: Fundamental3

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+let a = undefined;
+let b = ' ';
+let c = !b;
+
+let d = Boolean(a)
+console.log(d)          // false เพราะ undefined ถูกแปลงเป็น Boolean
+console.log(typeof(d))           
+
+let e = Boolean(b)
+console.log(e)          // true เพราะ string แปลงเป็น Boolean
+console.log(typeof(e))          
+
+let f = Boolean(c)
+console.log(f)          // false เพราะ not string ถูกค่าแล้วแปลงเป็น Boolean
+console.log(typeof(f))  
